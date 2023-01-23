9cb0191db611da0e2747e474ea42591ea22680e5
Haebin <hbyeon@student.42seoul.kr>
2023-01-23 19:02:05 +0900

    2023/01/23 19:02

--- Spring_Principle1/src/main/java/hello/core/OrderApp.java
+++ Spring_Principle1/src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,18 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appconfig = new AppConfig();
+//        AppConfig appconfig = new AppConfig();
+//        MemberService memberService = appconfig.memberService();
+//        OrderService orderServic e = appconfig.orderService();
 
-        MemberService memberService = appconfig.memberService();
-        OrderService orderService = appconfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member1 = new Member(memberId, "memberA", Grade.VIP);
